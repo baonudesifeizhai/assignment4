afe07944a5db85de8e37a2d72a49c21aef96f480
baonudesifeizhai <baonudesifeizhai@github.com>
2022-04-05 21:15:52 +0800

    Comment for "A parameter is dead upon entry to a method but overwritten"

--- src/com/rain/dao/AdminDao.java
+++ src/com/rain/dao/AdminDao.java
@@ -90,6 +90,8 @@ public class AdminDao {
 			rs = stm.executeQuery();
 			if(rs.next()){
 				adminbean.setAid(rs.getInt("aid"));
+				// TODO Useless assignment for argument, the assignment is only valid in this method
+				// as the type of this argument is String, and this argument is never read in this method.
 				userID = rs.getString("userID");
 				adminbean.setUsername(rs.getString("username"));
 				adminbean.setName(rs.getString("name"));
