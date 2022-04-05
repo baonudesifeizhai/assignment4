0f2e8e8f673057c26d395cb104aa758247a67ee8
baonudesifeizhai <baonudesifeizhai@github.com>
2022-04-05 22:30:35 +0800

    Fix "Unwritten field"

--- src/com/rain/util/DBUtil.java
+++ src/com/rain/util/DBUtil.java
@@ -13,7 +13,6 @@ public class DBUtil {
 	public static String username = "root";
 	public static String password = "root";
 	public static String url = "jdbc:mysql://127.0.0.1:3306/books";
-	private static String location;
 	
 	static{
 		try {
@@ -33,11 +32,6 @@ public class DBUtil {
 		
 		} catch (SQLException e){
 			e.printStackTrace();
-			// FIXME This field has not been initiated at this point,
-			// so the call of any method of this field would cause NPE.
-			if(location.indexOf("-") >= 0) {
-				System.out.println(location.substring(0,location.indexOf("-")));
-			}
 		}
 		return conn;
 	}
